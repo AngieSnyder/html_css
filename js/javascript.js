c133f08fc8e4dbe93ae1d8fac422ff79da04733b
AngieSnyder <Angie.Snyder@protonmail.com>
2018-10-26 19:14:53 -0400

    updated

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 
 function clicked() {
-    var inputCrtl = document.getElementById("txt");
-    var inputValue = inputCrtl.value;
+    var inputCtrl = document.getElementById("txt");
+    var inputValue = inputCtrl.value;
     console.log("the value is", inputValue);
     // this is a comment
     var labelCtrl = document.getElementById("lbl");
